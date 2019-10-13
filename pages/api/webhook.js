a7ad03e54d1be7d72a3fefa4775e8dc3f172fece
Kevin Lohman <badpirate@gmail.com>
2019-10-13 15:10:53 -0700

    practice makes...

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -2,5 +2,5 @@ export default (req, res) => {
   let { query: { challenge }} = req
   res.setHeader('Content-Type','text/plain')
   res.setHeader('X-Content-Type-Options','nosniff')
-  res.status(200).text(challenge)
+  res.end(challenge)
 }
